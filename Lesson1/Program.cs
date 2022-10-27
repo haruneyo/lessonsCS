87d01c8f5a750bf0b8cf8b5303c8200a1d46d91d
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-10-27 18:30:15 +0500

    Added new code

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, " + Console.ReadLine() + "!");
+﻿Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+
+if (name.ToLower() == "meow")
+{
+    Console.WriteLine("Hello, Meow!!!");
+}
+else 
+{
+    Console.WriteLine("Hello, " + name);
+}
+
+int numberOne = new Random().Next(0, 100);
+int numberTwo = new Random().Next(0, 100);
+Console.WriteLine(numberOne);
+Console.WriteLine(numberTwo);
+Console.WriteLine(numberOne + numberTwo);
